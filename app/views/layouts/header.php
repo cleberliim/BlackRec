366e355ac773ffad6d8ee39e47c1a22e9ac83e34
cleberliim <cleberliim@outlook.com>
2024-11-11 08:25:05 -0300

    modified:   app/views/layouts/header.php  	renamed:    pages/contato.php -> pages/contact.php

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -5,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BlackRec</title>
 
+    <!-- Taiwindcss -->
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- Custom css -->
+    <link href="/public/assets/css/custom.css" rel="stylesheet" title="custom">
 
     
 </head>
