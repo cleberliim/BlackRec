ee5e08040e60fd878e1da7208f67cbb807ad9398
cleberliim <cleberliim@outlook.com>
2024-11-22 17:27:31 -0300

    modified:   app/views/layouts/app.php

--- app/views/layouts/app.php
+++ app/views/layouts/app.php
@@ -1,13 +1,7 @@
 <?php include_once("navbar.php");?>
-
 <?php include_once("header.php");?>
-
 <?php include_once("../BlackRec/pages/services.php");?>
-
 <?php include_once("../BlackRec/pages/brands.php");?>
-
 <?php include_once("../BlackRec/pages/portfolio.php");?>
-
 <?php include_once("../BlackRec/pages/contact.php");?>
-
 <?php include_once("footer.php");?>
