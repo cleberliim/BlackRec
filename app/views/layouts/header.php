e0917657ece62e85d7d525c69129095ac97ba381
cleberliim <cleberliim@outlook.com>
2024-11-22 16:14:59 -0300

    modified:   app/views/layouts/header.php

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -25,15 +25,12 @@
   <?php include_once("../BlackRec/app/views/layouts/navbar.php") ?>
 
   
-  <!-- Header Section -->
+ <!-- Botão e Modal -->
 <header class="relative h-screen bg-cover bg-center" style="background-image: url('http://localhost/BlackRec/public/img/background.png');">
   <div class="absolute inset-0 bg-black bg-opacity-80 flex flex-col items-center justify-center text-center px-4">
-    <!-- Texto -->
     <h1 class="text-white font-thin font-montserrat text-[20px] mb-8" style="letter-spacing: 0.5px;">
       Bem-vindo ao universo <span class="font-bold">Blackrec</span>, onde a arte ganha vida <br>através de vídeos autênticos e ousados.
     </h1>
-
-    <!-- Botão Play -->
     <button id="playButton" class="w-24 h-24 rounded-full bg-red-600 flex items-center justify-center hover:bg-red-700 transition duration-300">
       <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" class="w-10 h-10">
         <path d="M8 5v14l11-7z" />
@@ -42,13 +39,9 @@
   </div>
 </header>
 
-<!-- Modal -->
 <div id="videoModal" class="hidden fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50">
   <div class="relative w-full max-w-3xl mx-auto bg-black p-4 rounded-lg shadow-lg">
-    <!-- Botão Fechar -->
     <button id="closeModal" class="absolute top-2 right-2 text-white text-2xl hover:text-gray-300">&times;</button>
-    
-    <!-- Vídeo -->
     <div class="aspect-w-16 aspect-h-9">
       <iframe 
         id="videoIframe"
@@ -62,13 +55,12 @@
   </div>
 </div>
 
-<!-- JavaScript -->
 <script>
   const playButton = document.getElementById('playButton');
   const videoModal = document.getElementById('videoModal');
   const closeModal = document.getElementById('closeModal');
   const videoIframe = document.getElementById('videoIframe');
-  const videoUrl = "https://www.youtube.com/embed/SeuVideoAqui?autoplay=1"; // Substitua com o URL do vídeo
+  const videoUrl = "https://www.youtube.com/embed/AVKHkOQ5bYQ?autoplay=1";  //alterar a url do video
 
   playButton.addEventListener('click', () => {
     videoIframe.src = videoUrl; // Define o vídeo ao abrir o modal
